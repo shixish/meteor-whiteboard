3f414c19a178e4d17b09fab585ad0bbf2753a20a
Andrew <shixish@gmail.com>
2013-05-10 07:16:11 +0000

    All kinds of whack

--- leaderboard.js
+++ leaderboard.js
@@ -36,55 +36,43 @@ if (Meteor.isClient) {
     }
   });
   
+  //var mouse_down = false;
+  //var path;
+  //
   //Template.canvas.events({
-  //  'mousedown': function(){
-  //    //console.log("paper:", paper);
+  //  'mousedown': function(event){
+  //    console.log("down");
+  //    mouse_down = true;
+  //    path = new paper.Path({
+  //      segments: [event.point],
+  //      strokeColor: 'black',
+  //      //fullySelected: true
+  //    });
   //  },
-  //  'mouseup': function(){
-  //    
+  //  'mouseup': function(event){
+  //    console.log("up")
+  //    mouse_down = false;
+  //  },
+  //  'mousemove': function(event){
+  //    if (mouse_down)
+  //      path.add(event.point);
   //  }
   //});
-  Template.canvas.paths = function () {
-    console.log('re-render the paths');
-    Paths.findOne();//this tells meteor to watch for updates...
-    //var paths = Paths.find().fetch();
-    //console.log('paths:', paths);
-    //for(var p in paths){
-    //  //console.log(paths[p]);
-    //  new paper.Path(paths[p]);
-    //}
-  };
   
-  //Template.canvas.output = function(){
-  //  return paper.view._element;
-  //}
-  
-  Template.canvas.created = function(){
-    //this.canvas = document.createElement("canvas");
-    //paper.setup(this.canvas);
-  }
-  
-  Template.canvas.rendered = function(){
-    //Note: use Paths.find().fetch() to check out the db.
-    
-    // Get a reference to the canvas object
-    var canvas = this.firstNode;//document.getElementById('canvas');
-    // Create an empty project and a view for the canvas:
-    paper.setup(canvas);
-    
+  Template.whiteboard.paths = function () {
+    console.log('render the paths');
+    //Paths.findOne();//this tells meteor to watch for updates...
     var paths = Paths.find().fetch();
     for(var p in paths){
       new paper.Path(paths[p]);
     }
-    
+  };
+  
+  Template.canvas.init = function(){
+    console.log('init');
+    var canvas = document.getElementById('canvas');//document.createElement('canvas');
+    paper.setup(canvas);
     var path;
-    
-    //var textItem = new paper.PointText({
-    //  content: 'Click and drag to draw a line.',
-    //  point: new paper.Point(20, 30),
-    //  fillColor: 'black',
-    //});
-    
     paper.tool.onMouseDown = function(event) {
       // If we produced a path before, deselect it:
       if (path) {
@@ -94,7 +82,6 @@ if (Meteor.isClient) {
       path = new paper.Path({
         segments: [event.point],
         strokeColor: 'black',
-        // Select the path, so we can see its segment points:
         //fullySelected: true
       });
     }
@@ -103,25 +90,87 @@ if (Meteor.isClient) {
     // at the position of the mouse:
     paper.tool.onMouseDrag = function(event) {
       path.add(event.point);
-      // Update the content of the text item to show how many
-      // segments it has:
-      //textItem.content = 'Segment count: ' + path.segments.length;
     }
     
     // When the mouse is released, we simplify the path:
     paper.tool.onMouseUp = function(event) {
-      //var segmentCount = path.segments.length;
-      // When the mouse is released, simplify it:
       path.simplify(10);
-      // Select the path, so we can see its segments:
-      //path.fullySelected = true;
-      //Paths.push(path.toJSON());
       addPath(path);
-      //var newSegmentCount = path.segments.length;
-      //var difference = segmentCount - newSegmentCount;
-      //var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
-      //textItem.content = difference + ' of the ' + segmentCount + ' segments were removed. Saving ' + percentage + '%';
     }
+  }
+  
+  Meteor.startup(function(){
+    console.log('startup');
+  });
+  
+  Template.canvas.created = function(){
+    //this.canvas = document.createElement("canvas");
+    //paper.setup(this.canvas);
+  }
+  
+  Template.canvas.rendered = function(){
+    console.log('rendered')
+    //console.log(this.firstNode);
+    //window.t = this.firstNode;
+    
+    //this.firstNode.appendChild(canvas);
+    //Note: use Paths.find().fetch() to check out the db.
+    
+    // Get a reference to the canvas object
+    var canvas = this.firstNode;//document.getElementById('canvas');
+    //// Create an empty project and a view for the canvas:
+    paper.setup(canvas);
+    //
+    //var paths = Paths.find().fetch();
+    //for(var p in paths){
+    //  new paper.Path(paths[p]);
+    //}
+    //
+    //var path;
+    
+    //var textItem = new paper.PointText({
+    //  content: 'Click and drag to draw a line.',
+    //  point: new paper.Point(20, 30),
+    //  fillColor: 'black',
+    //});
+    //
+    //paper.tool.onMouseDown = function(event) {
+    //  // If we produced a path before, deselect it:
+    //  if (path) {
+    //      path.selected = false;
+    //  }
+    //  // Create a new path and set its stroke color to black:
+    //  path = new paper.Path({
+    //    segments: [event.point],
+    //    strokeColor: 'black',
+    //    // Select the path, so we can see its segment points:
+    //    //fullySelected: true
+    //  });
+    //}
+    //
+    //// While the user drags the mouse, points are added to the path
+    //// at the position of the mouse:
+    //paper.tool.onMouseDrag = function(event) {
+    //  path.add(event.point);
+    //  // Update the content of the text item to show how many
+    //  // segments it has:
+    //  //textItem.content = 'Segment count: ' + path.segments.length;
+    //}
+    //
+    //// When the mouse is released, we simplify the path:
+    //paper.tool.onMouseUp = function(event) {
+    //  //var segmentCount = path.segments.length;
+    //  // When the mouse is released, simplify it:
+    //  path.simplify(10);
+    //  // Select the path, so we can see its segments:
+    //  //path.fullySelected = true;
+    //  //Paths.push(path.toJSON());
+    //  addPath(path);
+    //  //var newSegmentCount = path.segments.length;
+    //  //var difference = segmentCount - newSegmentCount;
+    //  //var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
+    //  //textItem.content = difference + ' of the ' + segmentCount + ' segments were removed. Saving ' + percentage + '%';
+    //}
   }
   
   //Template.canvas.addPath = function(path){
