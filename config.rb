3c5a6a55b566d54d8f074d1dbdb3b1c7458f41ad
Andrew <shixish@gmail.com>
2014-01-23 23:22:00 -1000

    updated meteor to using the windows version, and changed from less to compass

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :scss
+#http_path = '/'
+css_dir = './'
+sass_dir = './'
+#images_dir = 'assets/images'
+#javascripts_dir = 'assets/javascripts'
+#relative_assets = true
+#line_comments = true
+#output_style = :compressed
